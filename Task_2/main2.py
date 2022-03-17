d2b54330c3bb20cfe0714a91857c9d7f716aab88
Petra12345 <78352813+Petra12345@users.noreply.github.com>
2022-03-17 15:50:32 +0100

    Add ncf.py file

--- Task_2/main2.py
+++ Task_2/main2.py
@@ -9,4 +9,5 @@ ratings_small = create_small_practice_dataset(ratings)
 print('---------- data exploration -------------')
 data_exploration(ratings_small)
 
-
+#%%
+print('---------- pre-processing -----------')
