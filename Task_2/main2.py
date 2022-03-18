be746e78369123d9715a18b3432f250f8a96cd40
Petra12345 <78352813+Petra12345@users.noreply.github.com>
2022-03-18 10:49:08 +0100

    Add transformation to implicit data

--- Task_2/main2.py
+++ Task_2/main2.py
@@ -8,9 +8,11 @@ ratings = load_data()
 ratings_small = create_small_practice_dataset(ratings)
 
 #%%
-print('---------- data exploration -------------')
-data_exploration(ratings_small)
+# print('---------- data exploration -------------')
+# data_exploration(ratings_small)
 
 #%%
 print('---------- pre-processing -----------')
 train, test = train_test_split(ratings_small)
+train = transform_to_implicit(train)
+train_users, train_movies, train_labels = add_negatives(train, ratings_small)
--- Task_2/preprocessing.py
+++ Task_2/preprocessing.py
@@ -1,9 +1,13 @@
 # imports
 import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
 
 def train_test_split(ratings):
     # order per user the ratings s.t. timestamp is descending, then newest_rated = 1 for the newest ranked
     ratings['newest_rated'] = ratings.groupby(['userId'])['timestamp'].rank(method='first', ascending=False)
+    # @ sophie: deze regel geeft een warning maar dat is een vals positief in pandas, dus maakt niet uit
 
     # make train test split
     train = ratings[ratings['newest_rated'] != 1]
@@ -15,7 +19,40 @@ def train_test_split(ratings):
 
     return train, test
 
+
+def transform_to_implicit(train):
+    train.loc[:, 'rating'] = 1  # because these are the movies a user interacted with
+    return train
+
+
+def add_negatives(train, ratings):
+    num_negatives = 5  # TODO: now ratio 4:1, but maybe adjust
+    all_movies = ratings['movieId'].unique()  # get all different movie Ids from the whole data set
+    user_movie_set = set(zip(train['userId'], train['movieId']))  # make set with user-movie pairs
+
+    # create user, item and labels array
+    users = list()
+    movies = list()
+    labels = list()
+
+    #TODO: kan dit efficienter? Want het duurt lang
+    for user, movie in tqdm(user_movie_set):    # met tqdm krijg je zo'n cool progress balkje
+        users.append(user)
+        movies.append(movie)
+        labels.append(1)  # because user interacted with this model
+
+        # add negative examples
+        for _ in range(num_negatives):
+            negative_movie = np.random.choice(all_movies)
+            while (user, negative_movie) in user_movie_set:        # if user already interacted with picked movie
+                negative_movie = np.random.choice(all_movies)   # pick other movie
+            users.append(user)
+            movies.append(movie)
+            labels.append(0)        # because negative example
+
+    return users, movies, labels
+
 # TODO:
-# - one-hot-encode user id's
+# - one-hot-encode user id's?
 # - transform to implicit data
 # - train-test split
