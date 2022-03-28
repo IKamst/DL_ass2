535945588578b270807c4b42c1180089dc5be282
IKamst <iris@LAPTOP-F8HV9G39.localdomain>
2022-03-28 10:04:11 +0200

    tijdelijk voor testen GPU

--- Task_1/main1.py
+++ Task_1/main1.py
@@ -8,9 +8,9 @@ import data_handling
 import style_transfer_fast, style_transfer
 
 if __name__ == "__main__":
-    labels, images, min_x, min_y = data_analysis.perform_data_analysis()
-    train_x, test_x, train_y, test_y = data_handling.process_data(images, labels)
+    # labels, images, min_x, min_y = data_analysis.perform_data_analysis()
+    # train_x, test_x, train_y, test_y = data_handling.process_data(images, labels)
     # data_handling.load_data()
     # content_image, style_image = data_handling.load_data()
-    # style_transfer_fast.fast_transfer_style()
-    style_transfer.main_style_transfer(train_x, test_x, train_y)
+    style_transfer_fast.fast_transfer_style()
+    # style_transfer.main_style_transfer(train_x, test_x, train_y)
